71f9a1ec8a234b9e4719b05f2f997bc312dbaf92
Stephen Chapman <stephen.chapman@krisandbrake.com>
2015-03-12 23:03:09 -0700

    edit MyPiece

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -5,6 +5,20 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  # class array holding all the pieces and their rotations
+  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+  rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
+  [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
+  [[0, 0], [0, -1], [0, 1], [0, 2]]],
+  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+  rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
+  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+  #new shape - three-point
+  #rotations([[0, 0], [1,0], [0, 1]]), # new three-point
+  #new shape - five-point
+  #rotations([[0, 0], [1,0], [0, 1], [1,1], [2,1]]), # new five-point
+  
 
   # your enhancements here
 
