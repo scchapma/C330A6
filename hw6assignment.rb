1663e9b7c1c975039d78ae372d03767fd5841852
Stephen Chapman <stephen.chapman@krisandbrake.com>
2015-03-14 16:19:45 -0700

    edit constructors

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -4,33 +4,60 @@
 # part of your solution.
 
 class MyPiece < Piece
+  def initialize (point_array, board)
+    super(point_array, board)
+  end
+
   # The constant All_My_Pieces should be declared here
   # class array holding all the pieces and their rotations
-  All_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
+  All_My_Pieces = [[[[0, 0], [1, 0], [0, 1], [1, 1]]],  # square (only needs one)
   rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
   [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
   [[0, 0], [0, -1], [0, 1], [0, 2]]],
-  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+  #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+  #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
   rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]])] # Z
+  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
   #new shape - three-point
-  #rotations([[0, 0], [1,0], [0, 1]]), # new three-point
+  rotations([[0, 0], [1,0], [0, 1]])] # new three-point
   #new shape - five-point
   #rotations([[0, 0], [1,0], [0, 1], [1,1], [2,1]]), # new five-point
   
 
   # your enhancements here
+  # class method to choose the next piece
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 
 end
 
 class MyBoard < Board
   # your enhancements here
+  def initialize (game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  #store_current - amend for 3 and 5-block pieces
 
 end
 
 class MyTetris < Tetris
+  def initialize
+    super
+  end
+
   # your enhancements here
+
 
 end
 
