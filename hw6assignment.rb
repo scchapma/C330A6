a9ebb3400340999df7fbeb87182f196f6f1d1065
Stephen Chapman <stephen.chapman@krisandbrake.com>
2015-03-14 17:17:28 -0700

    add 'u' key

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -5,7 +5,6 @@
 
 class MyPiece < Piece
   def initialize (point_array, board)
-    puts ("Enter MyPiece constructor.")
     super(point_array, board)
   end
 
@@ -15,46 +14,62 @@ class MyPiece < Piece
   rotations([[0, 0], [-1, 0], [1, 0], [0, -1]]), # T
   [[[0, 0], [-1, 0], [1, 0], [2, 0]], # long (only needs two)
   [[0, 0], [0, -1], [0, 1], [0, 2]]],
-  #rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
-  #rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
+  rotations([[0, 0], [0, -1], [0, 1], [1, 1]]), # L
+  rotations([[0, 0], [0, -1], [0, 1], [-1, 1]]), # inverted L
   rotations([[0, 0], [-1, 0], [0, -1], [1, -1]]), # S
-  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z
-  #new shape - three-point
-  rotations([[0, 0], [1,0], [0, 1]])] # new three-point
-  #new shape - five-point
-  #rotations([[0, 0], [1,0], [0, 1], [1,1], [2,1]]), # new five-point
+  rotations([[0, 0], [1, 0], [0, -1], [-1, -1]]), # Z 
+  #three new shapes
+  rotations([[0, 0], [-1,0], [1, 0], [2, 0], [-2, 0]]), #long-long
+  rotations([[0, 0], [1,0], [0, 1]]), # new three-point  
+  rotations([[0, -1], [1,-1], [0, 0], [1,0], [0,1]])] # square with appendage
   
-
   # your enhancements here
-  # class method to choose the next piece
-  def self.next_piece (board)
+  def self.next_piece (board)    
     MyPiece.new(All_My_Pieces.sample, board)
   end
 
 end
 
 class MyBoard < Board
-  # your enhancements here
   def initialize (game)
-    @grid = Array.new(num_rows) {Array.new(num_columns)}
-    @current_block = MyPiece.next_piece(self)
-    @score = 0
-    @game = game
-    @delay = 500
+    super(game)
+    @current_block = MyPiece.next_piece(self)    
   end
 
+  # your enhancements here
   def next_piece
     @current_block = MyPiece.next_piece(self)
     @current_pos = nil
   end
 
   #store_current - amend for 3 and 5-block pieces
+  def store_current
+    locations = @current_block.current_rotation
+    displacement = @current_block.position
+    last = @current_block.current_rotation.length - 1    
+    (0..last).each{|index| 
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] = 
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
+  end
+
+  # rotates the current piece 180 degrees
+  def rotate_180
+    if !game_over? and @game.is_running?
+      @current_block.move(0, 0, -2)
+    end
+    draw
+  end
 
 end
 
 class MyTetris < Tetris
   def initialize
     super
+    my_key_binding
   end
 
   def set_board
@@ -66,7 +81,9 @@ class MyTetris < Tetris
   end
 
   # your enhancements here
-
+  def my_key_binding 
+     @root.bind('u', lambda {@board.rotate_180})
+  end  
 
 end
 
